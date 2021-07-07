e268854b0e7cc98de0d5afecb19b54beae2c6884
aincicau <alexandru.incicau@gmail.com>
2021-07-07 11:45:25 +0300

    setting up servere

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"golangserver/rest"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
-	
+	http.HandleFunc("/v1/", rest.Welcome)
+	http.HandleFunc("/v1/bye", rest.Welcome)
+	chi := chi.NewRouter()
+	http.ListenAndServe(":8080", nil)
 }
